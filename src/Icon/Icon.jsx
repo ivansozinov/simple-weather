6dc7dc77a3e583d109ec8cb47cb316b6c1229a7a
Ivan Sozinov <ivansozinov@me.com>
2022-12-26 20:11:55 +0300

    night icons

--- src/Icon/Icon.jsx
+++ src/Icon/Icon.jsx
@@ -4,9 +4,12 @@ function getIconUrl(iconCode) {
     return ICON_MAP.get(iconCode)
 }
 
+const hours = new Date().getHours()
+const isDayTime = hours > 7 && hours < 20
+
 export default function Icon(props) {
     const {iconCode} = props;
     return (
-        <div className='weather-icon' style={{ backgroundImage: `url(/weather_icons/${getIconUrl(iconCode)}.svg)` }}></div>
+        <div className='weather-icon' style={{ backgroundImage: `url(/weather_icons/${isDayTime ? '' : 'nt_'}${getIconUrl(iconCode)}.svg)` }}></div>
     )
 }
--- src/Icon/weatherIconMap.js
+++ src/Icon/weatherIconMap.js
@@ -4,10 +4,7 @@ addMapping([0, 1], "sunny")
 addMapping([2], "mostlycloudy")
 addMapping([3], "cloudy")
 addMapping([45, 48], "fog")
-addMapping(
-  [51, 53, 55, 56, 57, 61, 63, 65, 66, 67, 80, 81, 82],
-  "rain"
-)
+addMapping([51, 53, 55, 56, 57, 61, 63, 65, 66, 67, 80, 81, 82], "rain")
 addMapping([71, 73, 75], "sleet")
 addMapping([77, 85, 86], "snow")
 addMapping([95], "chancetstorms")
